c325ecb4b03edb43e36b63c6e4dcb58bec53e8ba
Julia Zajusz <julia.zajusz@fingo.pl>
2019-12-14 13:22:44 +0100

    bit of refactor

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,12 +1,9 @@
 'use strict';
 
 
-import {greedyMKP, isFibonacci} from "./teskUtils";
-import {getPersonCapacityById, getPersonOccupation, getPersonUsedCapacity} from "./personController";
-import * as _ from 'lodash'
+import {isFibonacci, setTaskAssignee, shuffleOpenTasks} from "./teskUtils";
 
 const mongoose = require('mongoose');
-const Person = mongoose.model('Person');
 const Task = mongoose.model('Task');
 
 
@@ -22,26 +19,6 @@ exports.list_all_tasks = function (req, res) {
   });
 };
 
-const getLeastOccupiedPerson = async () => {
-  let all_persons = [];
-  await Person.find({}, function (err, persons) {
-    if (err)
-      throw err;
-    all_persons = persons;
-  });
-
-  const personsOccupationArray = [];
-  for (let i = 0; i < all_persons.length; i++) {
-    personsOccupationArray.push({
-      personId: all_persons[i]._id,
-      occupation: await getPersonOccupation(all_persons[i]._id)
-    });
-  }
-  const chosenPersonOccupation = _.minBy(personsOccupationArray, 'occupation');
-  console.log('personsOccupationArray', personsOccupationArray);
-  console.log('chosenPersonOccupation', chosenPersonOccupation);
-  return chosenPersonOccupation;
-}
 
 exports.create_a_task = async function (req, res) {
   let new_task = new Task(req.body);
@@ -50,24 +27,7 @@ exports.create_a_task = async function (req, res) {
     res.send('size is not a Fibonacci number');
     return;
   }
-  let chosenPersonOccupation = null;
-  for (let i = 0; i < 2; i++) {
-    chosenPersonOccupation = await getLeastOccupiedPerson();
-    let person = await getPersonCapacityById(chosenPersonOccupation.personId);
-    if (chosenPersonOccupation.occupation + size > person.capacity) {
-      console.log('if');
-      try {
-        await shuffleOpenTasks();
-      } catch (err) {
-        console.log(err);
-      }
-      new_task.assignee = null;
-    } else {
-      console.log('else');
-      new_task.assignee = chosenPersonOccupation.personId;
-      break;
-    }
-  }
+  await setTaskAssignee(new_task);
   new_task.save(function (err, task) {
     if (err) {
       res.send(err);
@@ -111,32 +71,7 @@ exports.delete_a_task = function (req, res) {
   });
 };
 
-export const shuffleOpenTasks = async () => {
-  let all_tasks = await Task.find({status: "Open"}, function (err, tasks) {
-    if (err) throw err;
-  });
-  let all_people = await Person.find({}, function (err, persons) {
-    if (err) throw err;
-  });
 
-  const all_people_with_last_capacity = [];
-  for (let i = 0; i < all_people.length; i++) {
-    let uc = await getPersonUsedCapacity(all_people[i]._id);
-    const personWithLastCapacity = {...all_people[i].toObject(), capacity: all_people[i].capacity - uc, uc: uc};
-    all_people_with_last_capacity.push(personWithLastCapacity)
-  }
-  console.log(all_people_with_last_capacity);
-  let S = greedyMKP(all_tasks, all_people_with_last_capacity);
-  console.log(S);
-  for (let i = 0; i < S.length; i++) {
-    Task.findOneAndUpdate({_id: S[i]._id}, S[i], {new: true}, function (err, task) {
-      if (err) throw err;
-    });
-  }
-  console.log('exit shuffleOpenTasks');
-  return S;
-
-};
 
 exports.shuffle_open_tasks = async function (req, res) {
   let S = [];
--- controllers/teskUtils.js
+++ controllers/teskUtils.js
@@ -1,3 +1,11 @@
+import {getPersonCapacityById, getPersonOccupation, getPersonUsedCapacity} from "./personController";
+import * as mongoose from 'mongoose';
+import * as _ from "lodash";
+import {logger} from "../logger";
+
+const Person = mongoose.model('Person');
+const Task = mongoose.model('Task');
+
 export function isFibonacci(n) {
   const a = (5 * Math.pow(n, 2) + 4);
   const b = (5 * Math.pow(n, 2) - 4);
@@ -5,16 +13,7 @@ export function isFibonacci(n) {
   const result = Math.sqrt(a) % 1 === 0;
   const res = Math.sqrt(b) % 1 === 0;
 
-  //fixed this line
-  if (result || res === true) // checks the given input is fibonacci series
-  {
-    // let fib = Math.round(n * 1.618); // finds the next fibonacci series of given input
-    // console.log("The next Fibonacci number is " + fib);
-    return true;
-  } else {
-    // console.log(`The given number ${n} is not a fibonacci number`);
-    return false;
-  }
+  return result || res === true;
 }
 
 
@@ -78,4 +77,81 @@ export const greedyMKP = (tasks, people) => {
   }
   // console.log(S);
   return S;
+};
+
+
+export const shuffleOpenTasks = async () => {
+  let all_tasks = await Task.find({status: "Open"}, function (err, tasks) {
+    if (err) throw err;
+  });
+  let all_people = await Person.find({}, function (err, persons) {
+    if (err) throw err;
+  });
+
+  const all_people_with_last_capacity = [];
+  for (let i = 0; i < all_people.length; i++) {
+    let uc = await getPersonUsedCapacity(all_people[i]._id);
+    const personWithLastCapacity = {...all_people[i].toObject(), capacity: all_people[i].capacity - uc, uc: uc};
+    all_people_with_last_capacity.push(personWithLastCapacity)
+  }
+  let S = greedyMKP(all_tasks, all_people_with_last_capacity);
+  for (let i = 0; i < S.length; i++) {
+    Task.findOneAndUpdate({_id: S[i]._id}, S[i], {new: true}, function (err, task) {
+      if (err) {
+        logger.log({level: 'error', message: 'shuffle tasks error', error: err});
+        throw err;
+      }
+    });
+  }
+  return S;
+};
+
+
+export const getLeastOccupiedPerson = async (size) => {
+  let all_persons = [];
+  await Person.find({}, function (err, persons) {
+    if (err)
+      throw err;
+    all_persons = persons;
+  });
+
+  const personsOccupationArray = [];
+  for (let i = 0; i < all_persons.length; i++) {
+    let uc = await getPersonUsedCapacity(all_persons[i]._id);
+    let personWithCapacity = await getPersonCapacityById(all_persons[i]._id);
+    let occupation = await getPersonOccupation(all_persons[i]._id);
+    personsOccupationArray.push({
+      personId: all_persons[i]._id,
+      occupation: occupation,
+      last_capacity: personWithCapacity.capacity - uc
+    });
+  }
+  const chosenPersonOccupation = _.maxBy(personsOccupationArray, (item) => {
+    if (item.last_capacity - item.occupation > size) {
+      return item.last_capacity - item.occupation;
+    }
+  });
+  return chosenPersonOccupation;
+}
+
+
+export const setTaskAssignee = async (new_task) => {
+  for (let i = 0; i < 2; i++) {
+    let chosenPersonOccupation = await getLeastOccupiedPerson(new_task.size);
+    let person = null;
+    if (chosenPersonOccupation) {
+      person = await getPersonCapacityById(chosenPersonOccupation.personId);
+    }
+    if (chosenPersonOccupation && person && chosenPersonOccupation.occupation + new_task.size < person.capacity) {
+      new_task.assignee = chosenPersonOccupation.personId;
+      break;
+    } else {
+      try {
+        await shuffleOpenTasks();
+      } catch (err) {
+        logger.log({level: 'error', message: 'shuffle tasks error', error: err});
+      }
+      new_task.assignee = null;
+    }
+  }
 };
